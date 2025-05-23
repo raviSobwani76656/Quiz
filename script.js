b5b0fb3eb7ef1597ceed17ad8ca21f2d808d25a0
Ravi Sobwani <sobwaniravi@gmail.com>
2025-05-23 17:43:50 +0530

    styled the Quiz Page

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// document.body.style.backgroundColor = "red";
+
+const questionAndAnswers = [
+
+    {
+        question: 'What is 2+2',
+        Options: ["1", '2', '3', '4'],
+        result: '4'
+    },
+
+    {
+        question: "First man to walk on the moon",
+        Options: ['Neil ArmStrong',"Neil Degrass",'Eddy REbamance','oppo Tedo'
+        ]
+    }
+
+
+
+
+]
